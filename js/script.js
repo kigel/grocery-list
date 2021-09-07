54648631dbd9bbe7651ca68d4d9d48b2febf04d7
kigel <59475080+kigel@users.noreply.github.com>
2021-09-07 23:19:55 +0300

    added an amount counter to each list div

--- js/script.js
+++ js/script.js
@@ -17,12 +17,26 @@ function addToList() {
         divValue++;
         var newDiv = document.createElement("div");
         var newSpan = document.createElement("span");
+        var newSpanNumX = document.createElement("span");
+        var newSpanNum = document.createElement("span");
+        var newBtnPlus = document.createElement("button");
+        var newBtnMinus = document.createElement("button");
+        var newSpanBox = document.createElement("div");
         var newBtnBox = document.createElement("div");
         var newBtnRemove = document.createElement("button");
         var newBtnEdit = document.createElement("button");
         var inputValue = input.value;
 
         newSpan.textContent = inputValue;
+        newSpan.classList = "span";
+        newSpanNum.textContent = 1;
+        newSpanNum.classList = "spanNum";
+        newSpanNumX.textContent = "x";
+        newSpanBox.classList = "spanBox";
+        newBtnMinus.classList = "btnMinus" + " btnSpan";
+        newBtnMinus.textContent = "-"
+        newBtnPlus.textContent = "+"
+        newBtnPlus.classList = "btnPlus" + " btnSpan";
         newBtnBox.id = "btnBox"
         newBtnRemove.textContent = "REMOVE";
         newBtnRemove.classList = "listBtn" + " removeBtn" + " btn";
@@ -31,13 +45,28 @@ function addToList() {
         newDiv.id = "div" + divValue;
         newDiv.classList = "listDiv";
 
+        newSpanBox.appendChild(newBtnMinus);
+        newSpanBox.appendChild(newSpanNumX);
+        newSpanBox.appendChild(newSpanNum);
+        newSpanBox.appendChild(newBtnPlus);
         newDiv.appendChild(newSpan);
         newDiv.appendChild(newBtnBox);
+        newBtnBox.appendChild(newSpanBox);
         newBtnBox.appendChild(newBtnEdit);
         newBtnBox.appendChild(newBtnRemove);
         listBox.appendChild(newDiv);
     } else {
         alert("You can't add an empty value!");
+    }
+
+    for (var i = 0; i < listBox.querySelectorAll(".btnMinus").length; i++) {
+        listBox.querySelectorAll(".btnMinus")[i].addEventListener("click",
+        downTheSpan);
+    }
+
+    for (var i = 0; i < listBox.querySelectorAll(".btnPlus").length; i++) {
+        listBox.querySelectorAll(".btnPlus")[i].addEventListener("click",
+        upTheSpan);
     }
 
     for (var i = 0; i < listBox.querySelectorAll(".removeBtn").length; i++) {
@@ -51,6 +80,17 @@ function addToList() {
     }
 
     input.value = "";
+}
+
+function upTheSpan(e) {
+    console.log("+", e.currentTarget.parentNode.querySelector(".spanNum"));
+    e.currentTarget.parentNode.querySelector(".spanNum").textContent++;
+}
+
+function downTheSpan(e) {
+    if (e.currentTarget.parentNode.querySelector(".spanNum").textContent > 1) {
+        e.currentTarget.parentNode.querySelector(".spanNum").textContent--;
+    }
 }
 
 function removeFromList(e) {
